c9e04e52e303adf52a9c3ceedcc9cd32fd118d79
Enrico Bottazzi <85900164+enricobottazzi@users.noreply.github.com>
2023-05-11 10:45:16 +0200

    add support for `generate_setup_params` in bench

--- benches/full_solvency_flow.rs
+++ benches/full_solvency_flow.rs
@@ -1,6 +1,9 @@
 use circuits_halo2::{
-    merkle_sum_tree::{MerkleSumTree},
-    circuits::utils::{instantiate_empty_circuit, instantiate_circuit, full_prover, full_verifier}
+    circuits::utils::{
+        full_prover, full_verifier, generate_setup_params, instantiate_circuit,
+        instantiate_empty_circuit,
+    },
+    merkle_sum_tree::MerkleSumTree,
 };
 use criterion::{criterion_group, criterion_main, Criterion};
 use halo2_proofs::{
@@ -8,10 +11,9 @@ use halo2_proofs::{
     plonk::{keygen_pk, keygen_vk},
     poly::kzg::commitment::ParamsKZG,
 };
-use rand::rngs::OsRng;
+use std::convert::TryInto;
 
-
-const MAX_POWER: u32 = 10;
+const MAX_POWER: u32 = 27;
 const SAMPLE_SIZE: usize = 10;
 
 fn build_mstree_benchmark(_c: &mut Criterion) {
@@ -30,42 +32,39 @@ fn build_mstree_benchmark(_c: &mut Criterion) {
     }
 }
 
-// TO DO: replace params with a universal trusted setup
-// TO DO: figure out when k has to be increased
 fn verification_key_gen_benchmark(_c: &mut Criterion) {
-    let mut criterion = Criterion::default().sample_size(SAMPLE_SIZE);    
+    let mut criterion = Criterion::default().sample_size(SAMPLE_SIZE);
 
     for i in 4..=MAX_POWER {
+        let params: ParamsKZG<Bn256> = generate_setup_params(i.try_into().unwrap());
 
-        let k = 9; // 2^k is the number of rows for the circuit. For a merkle tree in the range between [4 levels (16 entries) and 10 levels (1024), we need 2^9 rows
-
-        // we generate a universal trusted setup of our own for testing
-        let params = ParamsKZG::<Bn256>::setup(k, OsRng);
         let bench_name = format!("gen verification key for 2 power of {} entries", i);
         criterion.bench_function(&bench_name, |b| {
             b.iter(|| {
-                keygen_vk(&params, &instantiate_empty_circuit(i.try_into().unwrap())).expect("vk generation should not fail");
+                keygen_vk(&params, &instantiate_empty_circuit(i.try_into().unwrap()))
+                    .expect("vk generation should not fail");
             })
         });
     }
 }
 
-// TO DO: replace params with a universal trusted setup
-// TO DO: figure out when k has to be increased
 fn proving_key_gen_benchmark(_c: &mut Criterion) {
-    let mut criterion = Criterion::default().sample_size(SAMPLE_SIZE);    
+    let mut criterion = Criterion::default().sample_size(SAMPLE_SIZE);
 
     for i in 4..=MAX_POWER {
+        let params: ParamsKZG<Bn256> = generate_setup_params(i.try_into().unwrap());
 
-        let k = 9; // 2^k is the number of rows for the circuit. For a merkle tree in the range between [4 levels (16 entries) and 10 levels (1024), we need 2^9 rows
-
-        // we generate a universal trusted setup of our own for testing
-        let params = ParamsKZG::<Bn256>::setup(k, OsRng);
-        let vk = keygen_vk(&params, &instantiate_empty_circuit(i.try_into().unwrap())).expect("vk generation should not fail");
+        let vk = keygen_vk(&params, &instantiate_empty_circuit(i.try_into().unwrap()))
+            .expect("vk generation should not fail");
         let bench_name = format!("gen proving key for 2 power of {} entries", i);
         criterion.bench_function(&bench_name, |b| {
             b.iter(|| {
-                keygen_pk(&params, vk.clone(), &instantiate_empty_circuit(i.try_into().unwrap())).expect("pk generation should not fail");
+                keygen_pk(
+                    &params,
+                    vk.clone(),
+                    &instantiate_empty_circuit(i.try_into().unwrap()),
+                )
+                .expect("pk generation should not fail");
             })
         });
     }
@@ -74,15 +73,10 @@ fn proving_key_gen_benchmark(_c: &mut Criterion) {
 fn generate_zk_proof_benchmark(_c: &mut Criterion) {
     let mut criterion = Criterion::default().sample_size(SAMPLE_SIZE);
 
-
     for i in 4..=MAX_POWER {
+        let circuit = instantiate_empty_circuit(i.try_into().unwrap());
 
-        let levels = 4;
-
-        let circuit = instantiate_empty_circuit(levels);
-
-        // we generate a universal trusted setup of our own for testing
-        let params = ParamsKZG::<Bn256>::setup(9, OsRng);
+        let params: ParamsKZG<Bn256> = generate_setup_params(i.try_into().unwrap());
 
         let vk = keygen_vk(&params, &circuit).expect("vk generation should not fail");
         let pk = keygen_pk(&params, vk.clone(), &circuit).expect("pk generation should not fail");
@@ -118,17 +112,12 @@ fn generate_zk_proof_benchmark(_c: &mut Criterion) {
 }
 
 fn verify_zk_proof_benchmark(_c: &mut Criterion) {
-
     let mut criterion = Criterion::default().sample_size(SAMPLE_SIZE);
 
     for i in 4..=MAX_POWER {
+        let circuit = instantiate_empty_circuit(i.try_into().unwrap());
 
-        let levels = 4;
-
-        let circuit = instantiate_empty_circuit(levels);
-
-        // we generate a universal trusted setup of our own for testing
-        let params = ParamsKZG::<Bn256>::setup(9, OsRng);
+        let params: ParamsKZG<Bn256> = generate_setup_params(i.try_into().unwrap());
 
         let vk = keygen_vk(&params, &circuit).expect("vk generation should not fail");
         let pk = keygen_pk(&params, vk.clone(), &circuit).expect("pk generation should not fail");
@@ -165,5 +154,12 @@ fn verify_zk_proof_benchmark(_c: &mut Criterion) {
     }
 }
 
-criterion_group!(benches, build_mstree_benchmark, verification_key_gen_benchmark, proving_key_gen_benchmark, generate_zk_proof_benchmark, verify_zk_proof_benchmark);
+criterion_group!(
+    benches,
+    build_mstree_benchmark,
+    verification_key_gen_benchmark,
+    proving_key_gen_benchmark,
+    generate_zk_proof_benchmark,
+    verify_zk_proof_benchmark
+);
 criterion_main!(benches);
