e3001da382efa511fae4efc47c375451d59acebf
Enrico Bottazzi <85900164+enricobottazzi@users.noreply.github.com>
2023-05-12 16:52:03 +0200

    fix: add condition to build tree sequentially after a certain level

--- src/merkle_sum_tree/utils/build_tree.rs
+++ src/merkle_sum_tree/utils/build_tree.rs
@@ -47,33 +47,50 @@ pub fn build_merkle_tree_from_entries(
 
     end_timer!(pf_time);
 
-    // Compute the inner nodes
+    // If the level is within the last {parallelization_threshold} levels, compute the nodes sequentially
+    // otherwise, compute the nodes in parallel
     for level in 1..=depth {
         let nodes_in_level = (n + (1 << level) - 1) / (1 << level);
-        let mut handles = vec![];
-        let chunk_size = (nodes_in_level + num_cpus::get() - 1) / num_cpus::get();
-        let pf_time = start_timer!(|| "compute middle level");
 
-        for chunk in tree[level - 1].chunks(chunk_size * 2) {
-            let chunk = chunk.to_vec();
-            handles.push(thread::spawn(move || {
-                chunk
-                    .chunks(2)
-                    .map(|pair| create_middle_node(&pair[0], &pair[1]))
-                    .collect::<Vec<_>>()
-            }));
-        }
+        let parallelization_threshold = num_cpus::get();
 
-        let mut index = 0;
-        for handle in handles {
-            let result = handle.join().unwrap();
-            for node in result {
-                tree[level][index] = node;
-                index += 1;
+        if level > depth - parallelization_threshold {
+            let pf_time = start_timer!(|| "compute middle level");
+
+            for i in 0..nodes_in_level {
+                tree[level][i] =
+                    create_middle_node(&tree[level - 1][2 * i], &tree[level - 1][2 * i + 1]);
             }
-        }
 
-        end_timer!(pf_time);
+            end_timer!(pf_time);
+            continue;
+        } else {
+            let mut handles = vec![];
+            let chunk_size = (nodes_in_level + num_cpus::get() - 1) / num_cpus::get();
+            let pf_time = start_timer!(|| "compute middle level");
+
+            for chunk in tree[level - 1].chunks(chunk_size * 2) {
+                let chunk = chunk.to_vec();
+                handles.push(thread::spawn(move || {
+                    chunk
+                        .chunks(2)
+                        .map(|pair| create_middle_node(&pair[0], &pair[1]))
+                        .collect::<Vec<_>>()
+                }));
+            }
+
+            let mut index = 0;
+            for handle in handles {
+                let result = handle.join().unwrap();
+                for node in result {
+                    tree[level][index] = node;
+                    index += 1;
+                }
+            }
+
+            end_timer!(pf_time);
+            continue;
+        }
     }
 
     let root = tree[depth][0].clone();
