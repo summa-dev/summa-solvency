a7f5a9bbfb7b8693f66f775535ac94341a79ec40
Enrico Bottazzi <85900164+enricobottazzi@users.noreply.github.com>
2023-05-15 14:54:31 +0200

    fix: remove use of `parallelization_threshold` variable
    The `parallelization_threshold` variable was added because I was afraid that building in parallel levels with a only a few hashing (the ones when you get close to the merkle root) would actually downgrade the performance of the library. But that's not the case so I removed this. Now all the levels inside a tree will be built in parallel

--- src/merkle_sum_tree/utils/build_tree.rs
+++ src/merkle_sum_tree/utils/build_tree.rs
@@ -65,52 +65,31 @@ pub fn build_merkle_tree_from_entries(
     for level in 1..=depth {
         let nodes_in_level = (n + (1 << level) - 1) / (1 << level);
 
-        // compute parallelization until {parallelization_threshold} level
-        // if parallelization_threshold is set to depth, then no parallelization
-        // if parallelization_threshold is set to 0, then all levels are parallelized (until level 0)
-        // if depth is smaller than 5, disable parallelization
-        // otherwise, only parallelize the initial 5 levels
-        let parallelization_threshold = if depth < 5 { depth } else { depth - 5 };
+        let mut handles = vec![];
+        let chunk_size = (nodes_in_level + num_cpus::get() - 1) / num_cpus::get();
+        let pf_time = start_timer!(|| "compute middle level in parallel");
+        print!("level {} ", level);
 
-        if level > depth - parallelization_threshold {
-            let pf_time = start_timer!(|| "compute middle level sequentially");
-            print!("{} ", depth - level);
-
-            for i in 0..nodes_in_level {
-                tree[level][i] =
-                    create_middle_node(&tree[level - 1][2 * i], &tree[level - 1][2 * i + 1]);
-            }
-
-            end_timer!(pf_time);
-            continue;
-        } else {
-            let mut handles = vec![];
-            let chunk_size = (nodes_in_level + num_cpus::get() - 1) / num_cpus::get();
-            let pf_time = start_timer!(|| "compute middle level in parallel");
-            print!("{} ", depth - level);
-
-            for chunk in tree[level - 1].chunks(chunk_size * 2) {
-                let chunk = chunk.to_vec();
-                handles.push(thread::spawn(move || {
-                    chunk
-                        .chunks(2)
-                        .map(|pair| create_middle_node(&pair[0], &pair[1]))
-                        .collect::<Vec<_>>()
-                }));
-            }
-
-            let mut index = 0;
-            for handle in handles {
-                let result = handle.join().unwrap();
-                for node in result {
-                    tree[level][index] = node;
-                    index += 1;
-                }
-            }
-
-            end_timer!(pf_time);
-            continue;
+        for chunk in tree[level - 1].chunks(chunk_size * 2) {
+            let chunk = chunk.to_vec();
+            handles.push(thread::spawn(move || {
+                chunk
+                    .chunks(2)
+                    .map(|pair| create_middle_node(&pair[0], &pair[1]))
+                    .collect::<Vec<_>>()
+            }));
         }
+
+        let mut index = 0;
+        for handle in handles {
+            let result = handle.join().unwrap();
+            for node in result {
+                tree[level][index] = node;
+                index += 1;
+            }
+        }
+
+        end_timer!(pf_time);
     }
 
     let root = tree[depth][0].clone();
