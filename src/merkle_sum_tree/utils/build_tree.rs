238d75078b1c67336579584c49a87f1509951442
Enrico Bottazzi <85900164+enricobottazzi@users.noreply.github.com>
2023-05-15 15:15:30 +0200

    refactor: created helper function for `build_leaves_level` and  `build_middle_level`
    The functions were previously defined inside the main `build_merkle_tree_from_entries` function. Now these 2 have been separated to make the code cleaner and simpler to read

--- src/merkle_sum_tree/utils/build_tree.rs
+++ src/merkle_sum_tree/utils/build_tree.rs
@@ -36,8 +36,20 @@ pub fn build_merkle_tree_from_entries(
         ]);
     }
 
-    let leaves_building = start_timer!(|| "compute leaves");
+    build_leaves_level(entries, &mut tree);
 
+    for level in 1..=depth {
+        build_middle_level(level, &mut tree, n)
+    }
+    end_timer!(tree_building);
+
+    let root = tree[depth][0].clone();
+    *nodes = tree;
+    Ok(root)
+}
+
+fn build_leaves_level(entries: &[Entry], tree:  &mut [Vec<Node>]) {
+    let leaves_building = start_timer!(|| "compute leaves");
     // Compute the leaves in parallel
     let mut handles = vec![];
     let chunk_size = (entries.len() + num_cpus::get() - 1) / num_cpus::get();
@@ -59,42 +71,35 @@ pub fn build_merkle_tree_from_entries(
             index += 1;
         }
     }
-
     end_timer!(leaves_building);
+}
 
-    for level in 1..=depth {
-        let nodes_in_level = (n + (1 << level) - 1) / (1 << level);
+fn build_middle_level(level: usize, tree: &mut [Vec<Node>], n: usize) {
+    let nodes_in_level = (n + (1 << level) - 1) / (1 << level);
 
-        let mut handles = vec![];
-        let chunk_size = (nodes_in_level + num_cpus::get() - 1) / num_cpus::get();
-        let middle_level_building = start_timer!(|| "compute middle level in parallel");
-        print!("{} ", level);
+    let mut handles = vec![];
+    let chunk_size = (nodes_in_level + num_cpus::get() - 1) / num_cpus::get();
+    let middle_level_building = start_timer!(|| "compute middle level in parallel");
+    print!("{} ", level);
 
-        for chunk in tree[level - 1].chunks(chunk_size * 2) {
-            let chunk = chunk.to_vec();
-            handles.push(thread::spawn(move || {
-                chunk
-                    .chunks(2)
-                    .map(|pair| create_middle_node(&pair[0], &pair[1]))
-                    .collect::<Vec<_>>()
-            }));
-        }
-
-        let mut index = 0;
-        for handle in handles {
-            let result = handle.join().unwrap();
-            for node in result {
-                tree[level][index] = node;
-                index += 1;
-            }
-        }
-
-        end_timer!(middle_level_building);
+    for chunk in tree[level - 1].chunks(chunk_size * 2) {
+        let chunk = chunk.to_vec();
+        handles.push(thread::spawn(move || {
+            chunk
+                .chunks(2)
+                .map(|pair| create_middle_node(&pair[0], &pair[1]))
+                .collect::<Vec<_>>()
+        }));
     }
 
-    end_timer!(tree_building);
+    let mut index = 0;
+    for handle in handles {
+        let result = handle.join().unwrap();
+        for node in result {
+            tree[level][index] = node;
+            index += 1;
+        }
+    }
 
-    let root = tree[depth][0].clone();
-    *nodes = tree;
-    Ok(root)
+    end_timer!(middle_level_building);
 }
