9710a93d9cb1b86d0e2f0da02367db1d134f70df
Enrico Bottazzi <85900164+enricobottazzi@users.noreply.github.com>
2023-05-18 17:24:54 +0200

    feat: add `Spec2` for poseidon hasher with 2 input

--- src/chips/poseidon/mod.rs
+++ src/chips/poseidon/mod.rs
@@ -1,3 +1,4 @@
 pub mod hash;
+pub mod rate2_params;
 pub mod rate4_params;
 pub mod spec;
--- src/chips/poseidon/spec.rs
+++ src/chips/poseidon/spec.rs
@@ -1,6 +1,7 @@
 //! Specification for rate 4 Poseidon using the BN256 curve.
 //! Patterned after [halo2_gadgets::poseidon::primitives::P128Pow5T3]
 
+use crate::chips::poseidon::rate2_params;
 use crate::chips::poseidon::rate4_params;
 use halo2_gadgets::poseidon::primitives::*;
 use halo2_proofs::arithmetic::Field;
@@ -33,6 +34,35 @@ impl Spec<Fp, 5, 4> for Spec4 {
             rate4_params::ROUND_CONSTANTS[..].to_vec(),
             rate4_params::MDS,
             rate4_params::MDS_INV,
+        )
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Spec2;
+
+impl Spec<Fp, 3, 2> for Spec2 {
+    fn full_rounds() -> usize {
+        8
+    }
+
+    fn partial_rounds() -> usize {
+        57
+    }
+
+    fn sbox(val: Fp) -> Fp {
+        val.pow_vartime(&[5])
+    }
+
+    fn secure_mds() -> usize {
+        unimplemented!()
+    }
+
+    fn constants() -> (Vec<[Fp; 3]>, Mds<Fp, 3>, Mds<Fp, 3>) {
+        (
+            rate2_params::ROUND_CONSTANTS[..].to_vec(),
+            rate2_params::MDS,
+            rate2_params::MDS_INV,
         )
     }
 }
