a343f127c5468f775b4ffe88c3280e2ea9b06f7d
Alex Kuzmin <alex.kuzmin@proton.me>
2023-07-03 19:19:43 +0800

    Code review fixes

--- contracts/src/Summa.sol
+++ contracts/src/Summa.sol
@@ -21,18 +21,6 @@ contract Summa is Ownable {
     uint256 constant ETH_INPUTS = 1;
     uint256 constant ERC20_INPUTS = 1;
 
-    struct PublicInput {
-        string exchangeId;
-        address[] cexETHAddresses;
-        address[] cexERC20Addresses;
-        uint256[] cexETHBalances;
-        uint256[] cexERC20Balances;
-        address[] erc20ContractAddresses;
-        bytes[] cexEthAddressSignatures;
-        bytes[] cexERC20AddressSignatures;
-        uint256 mstRoot;
-    }
-
     Verifier private verifier;
 
     event ProofOfSolvencySubmitted(bytes32 indexed exchangeId);
@@ -42,66 +30,74 @@ contract Summa is Ownable {
     }
 
     function submitProofOfSolvency(
-        PublicInput memory publicInput,
+        string memory exchangeId,
+        address[] memory cexETHAddresses,
+        address[] memory cexERC20Addresses,
+        uint256[] memory cexETHBalances,
+        uint256[] memory cexERC20Balances,
+        address[] memory erc20ContractAddresses,
+        bytes[] memory cexEthAddressSignatures,
+        bytes[] memory cexERC20AddressSignatures,
+        uint256 mstRoot,
         bytes memory proof
     ) public {
         require(
-            publicInput.cexEthAddressSignatures.length == ETH_INPUTS &&
-                publicInput.cexETHAddresses.length == ETH_INPUTS &&
-                publicInput.cexETHBalances.length == ETH_INPUTS,
+            cexEthAddressSignatures.length == ETH_INPUTS &&
+                cexETHAddresses.length == ETH_INPUTS &&
+                cexETHBalances.length == ETH_INPUTS,
             "CEX ETH addresses, balances, and signatures count mismatch"
         );
 
         require(
-            publicInput.cexERC20AddressSignatures.length == ERC20_INPUTS &&
-                publicInput.cexERC20Addresses.length == ERC20_INPUTS &&
-                publicInput.erc20ContractAddresses.length == ERC20_INPUTS &&
-                publicInput.cexERC20Balances.length == ERC20_INPUTS,
+            cexERC20AddressSignatures.length == ERC20_INPUTS &&
+            cexERC20Addresses.length == ERC20_INPUTS &&
+            erc20ContractAddresses.length == ERC20_INPUTS &&
+            cexERC20Balances.length == ERC20_INPUTS,
             "CEX ERC20 addresses, balances, and signatures count mismatch"
         );
 
-        for (uint i = 0; i < publicInput.cexEthAddressSignatures.length; i++) {
+        for (uint i = 0; i < cexEthAddressSignatures.length; i++) {
             // Check that message is "summa proof of solvency {exchangeId}" and the signature is valid
             address recoveredPubKey = keccak256(
                 abi.encode(
                     "Summa proof of solvency for ",
-                    publicInput.exchangeId
+                    exchangeId
                 )
             ).toEthSignedMessageHash().recover(
-                    publicInput.cexEthAddressSignatures[i]
+                    cexEthAddressSignatures[i]
                 );
             require(
-                publicInput.cexETHAddresses[i] == recoveredPubKey,
+                cexETHAddresses[i] == recoveredPubKey,
                 "Invalid signer for ETH address"
             );
             require(
-                publicInput.cexETHBalances[i] <=
-                    publicInput.cexETHAddresses[i].balance,
+                cexETHBalances[i] <=
+                    cexETHAddresses[i].balance,
                 "Actual ETH balance less than the proven balance"
             );
         }
 
         for (
             uint i = 0;
-            i < publicInput.cexERC20AddressSignatures.length;
+            i < cexERC20AddressSignatures.length;
             i++
         ) {
             address recoveredPubKey = keccak256(
                 abi.encode(
                     "Summa proof of solvency for ",
-                    publicInput.exchangeId
+                    exchangeId
                 )
             ).toEthSignedMessageHash().recover(
-                    publicInput.cexERC20AddressSignatures[i]
+                    cexERC20AddressSignatures[i]
                 );
             require(
-                publicInput.cexERC20Addresses[i] == recoveredPubKey,
+                cexERC20Addresses[i] == recoveredPubKey,
                 "Invalid signer for ERC20 address"
             );
             require(
-                publicInput.cexERC20Balances[i] <=
-                    IERC20(publicInput.erc20ContractAddresses[i]).balanceOf(
-                        publicInput.cexERC20Addresses[i]
+                cexERC20Balances[i] <=
+                    IERC20(erc20ContractAddresses[i]).balanceOf(
+                        cexERC20Addresses[i]
                     ),
                 "Actual ERC20 balance less than the proven balance"
             );
@@ -109,20 +105,20 @@ contract Summa is Ownable {
 
         // Verify ZK proof
         uint256[] memory inputs = new uint256[](ETH_INPUTS + ERC20_INPUTS + 1);
-        inputs[0] = publicInput.mstRoot;
+        inputs[0] = mstRoot;
 
         for (uint i = 0; i < ETH_INPUTS; i++) {
-            inputs[i + 1] = publicInput.cexETHBalances[i];
+            inputs[i + 1] = cexETHBalances[i];
         }
 
         for (uint i = 0; i < ERC20_INPUTS; i++) {
-            inputs[i + ETH_INPUTS + 1] = publicInput.cexERC20Balances[i];
+            inputs[i + ETH_INPUTS + 1] = cexERC20Balances[i];
         }
 
         require(verifyZkProof(proof, inputs), "Invalid zk proof");
 
         emit ProofOfSolvencySubmitted(
-            keccak256(abi.encode(publicInput.exchangeId))
+            keccak256(abi.encode(exchangeId))
         );
     }
 
