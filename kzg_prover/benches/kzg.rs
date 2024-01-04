b9c96ac61ae9bb2d68b275f0caca173c24893125
sifnoc <sifnoc09@gmail.com>
2024-01-04 09:30:48 +0000

    chore: different configuration for demonstrate benchmark performance

--- kzg_prover/benches/kzg.rs
+++ kzg_prover/benches/kzg.rs
@@ -193,8 +193,10 @@ fn bench_kzg<const K: u32, const N_USERS: usize, const N_CURRENCIES: usize, cons
 fn criterion_benchmark(_c: &mut Criterion) {
     const N_CURRENCIES: usize = 2;
     const N_POINTS: usize = 3;
+
+    // Demonstrating that a higher value of K has a more significant impact on benchmark performance than the number of users
     {
-        const K: u32 = 17;
+        const K: u32 = 18;
         const N_USERS: usize = 16;
         bench_kzg::<K, N_USERS, N_CURRENCIES, N_POINTS>(
             format!("K = {K}, N_USERS = {N_USERS}, N_CURRENCIES = {N_CURRENCIES}").as_str(),
