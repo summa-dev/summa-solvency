04fb8d3d2cb3c891caca735a8029a9ce2085a14e
sifnoc <sifnoc09@gmail.com>
2024-03-07 12:01:13 +0000

    fix: replace N_ASSETS to N_CURRENCIES

--- kzg_prover/src/utils/dummy_entries.rs
+++ kzg_prover/src/utils/dummy_entries.rs
@@ -7,18 +7,18 @@ use crate::cryptocurrency::Cryptocurrency;
 use crate::entry::Entry;
 
 // This is for testing purposes with a large dataset instead of using a CSV file
-pub fn generate_dummy_entries<const N_ASSETS: usize>(
-    entries: &mut [Entry<N_ASSETS>],
+pub fn generate_dummy_entries<const N_CURRENCIES: usize>(
+    entries: &mut [Entry<N_CURRENCIES>],
     cryptocurrencies: &mut [Cryptocurrency],
 ) -> Result<(), Box<dyn Error>> {
-    // Ensure N_ASSETS is greater than 0.
-    if N_ASSETS == 0 {
-        return Err("N_ASSETS must be greater than 0".into());
+    // Ensure N_CURRENCIES is greater than 0.
+    if N_CURRENCIES == 0 {
+        return Err("N_CURRENCIES must be greater than 0".into());
     }
 
-    // Ensure the length of `cryptocurrencies` matches `N_ASSETS`.
-    if cryptocurrencies.len() != N_ASSETS {
-        return Err("cryptocurrencies length must be equal to N_ASSETS".into());
+    // Ensure the length of `cryptocurrencies` matches `N_CURRENCIES`.
+    if cryptocurrencies.len() != N_CURRENCIES {
+        return Err("cryptocurrencies length must be equal to N_CURRENCIES".into());
     }
 
     for (i, cryptocurrency) in cryptocurrencies.iter_mut().enumerate() {
@@ -33,7 +33,7 @@ pub fn generate_dummy_entries<const N_ASSETS: usize>(
 
         let username: String = (0..10).map(|_| rng.sample(Alphanumeric) as char).collect();
 
-        let balances: [BigUint; N_ASSETS] =
+        let balances: [BigUint; N_CURRENCIES] =
             std::array::from_fn(|_| BigUint::from(rng.gen_range(1000..90000) as u32));
 
         *entry = Entry::new(username, balances).expect("Failed to create entry");
@@ -51,46 +51,52 @@ mod tests {
     #[test]
     fn test_generate_random_entries() {
         const N_USERS: usize = 1 << 17;
-        const N_ASSETS: usize = 2;
+        const N_CURRENCIES: usize = 2;
 
         // Setup a buffer for entries and cryptocurrencies
-        let mut entries = vec![Entry::<N_ASSETS>::init_empty(); N_USERS];
-        let mut cryptocurrencies = vec![Cryptocurrency::init_empty(); N_ASSETS];
+        let mut entries = vec![Entry::<N_CURRENCIES>::init_empty(); N_USERS];
+        let mut cryptocurrencies = vec![Cryptocurrency::init_empty(); N_CURRENCIES];
 
         // Attempt to generate random entries
-        assert!(generate_dummy_entries::<N_ASSETS>(&mut entries, &mut cryptocurrencies).is_ok());
+        assert!(
+            generate_dummy_entries::<N_CURRENCIES>(&mut entries, &mut cryptocurrencies).is_ok()
+        );
 
         // Verify that entries are populated
         assert_eq!(entries.len(), N_USERS);
         for entry in entries {
             assert!(!entry.username().is_empty());
-            assert_eq!(entry.balances().len(), N_ASSETS);
+            assert_eq!(entry.balances().len(), N_CURRENCIES);
         }
     }
 
     #[test]
     fn test_asset_not_zero() {
         const N_USERS: usize = 1 << 17;
-        const N_ASSETS: usize = 0;
+        const N_CURRENCIES: usize = 0;
 
         // Setup a buffer for entries and cryptocurrencies
-        let mut entries = vec![Entry::<N_ASSETS>::init_empty(); N_USERS];
-        let mut cryptocurrencies = vec![Cryptocurrency::init_empty(); N_ASSETS];
+        let mut entries = vec![Entry::<N_CURRENCIES>::init_empty(); N_USERS];
+        let mut cryptocurrencies = vec![Cryptocurrency::init_empty(); N_CURRENCIES];
 
-        // `N_ASSETS` is zero, so this should fail
-        assert!(generate_dummy_entries::<N_ASSETS>(&mut entries, &mut cryptocurrencies).is_err());
+        // `N_CURRENCIES` is zero, so this should fail
+        assert!(
+            generate_dummy_entries::<N_CURRENCIES>(&mut entries, &mut cryptocurrencies).is_err()
+        );
     }
 
     #[test]
     fn test_wrong_cryptocurrencies() {
         const N_USERS: usize = 1 << 17;
-        const N_ASSETS: usize = 2;
+        const N_CURRENCIES: usize = 2;
 
         // Setup a buffer for entries and cryptocurrencies
-        let mut entries = vec![Entry::<N_ASSETS>::init_empty(); N_USERS];
-        let mut cryptocurrencies = vec![Cryptocurrency::init_empty(); N_ASSETS + 1];
+        let mut entries = vec![Entry::<N_CURRENCIES>::init_empty(); N_USERS];
+        let mut cryptocurrencies = vec![Cryptocurrency::init_empty(); N_CURRENCIES + 1];
 
-        // `cryptocurrencies` length is not equal to `N_ASSETS`, so this should fail
-        assert!(generate_dummy_entries::<N_ASSETS>(&mut entries, &mut cryptocurrencies).is_err());
+        // `cryptocurrencies` length is not equal to `N_CURRENCIES`, so this should fail
+        assert!(
+            generate_dummy_entries::<N_CURRENCIES>(&mut entries, &mut cryptocurrencies).is_err()
+        );
     }
 }
