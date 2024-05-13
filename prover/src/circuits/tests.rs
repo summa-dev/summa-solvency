62aaf8096db84fb7999f62bfe08f6348d57ec792
Alex Kuzmin <6849426+alxkzmn@users.noreply.github.com>
2024-05-13 19:08:02 +0800

    Verify user inclusion in a test (#290)
    * Verify user inclusion in a test
    
    * Perform batch KZG opening/verification
    
    * Remove magic number
    
    * Separate negative tests for invalid grand total and invalid range check

--- prover/src/circuits/tests.rs
+++ prover/src/circuits/tests.rs
@@ -1,79 +1,33 @@
+use halo2_proofs::arithmetic::Field;
 use plonkish_backend::{
-    backend::{hyperplonk::HyperPlonk, PlonkishBackend, PlonkishCircuit, PlonkishCircuitInfo},
+    backend::{hyperplonk::HyperPlonk, PlonkishBackend, PlonkishCircuit},
     frontend::halo2::Halo2Circuit,
     halo2_curves::bn256::{Bn256, Fr as Fp},
-    pcs::{multilinear::MultilinearKzg, PolynomialCommitmentScheme},
+    pcs::{multilinear::MultilinearKzg, Evaluation, PolynomialCommitmentScheme},
     util::{
-        arithmetic::PrimeField,
-        transcript::{InMemoryTranscript, Keccak256Transcript, TranscriptRead, TranscriptWrite},
-        DeserializeOwned, Serialize,
+        transcript::{
+            FieldTranscriptRead, FieldTranscriptWrite, InMemoryTranscript, Keccak256Transcript,
+        },
+        Itertools,
     },
     Error::InvalidSumcheck,
 };
-use std::hash::Hash;
 
 use rand::{
     rngs::{OsRng, StdRng},
-    CryptoRng, RngCore, SeedableRng,
+    CryptoRng, Rng, RngCore, SeedableRng,
 };
 
 use crate::{
-    circuits::summa_circuit::summa_hyperplonk::SummaHyperplonk, utils::generate_dummy_entries,
+    circuits::summa_circuit::summa_hyperplonk::SummaHyperplonk,
+    utils::{
+        big_uint_to_fp, fp_to_big_uint, generate_dummy_entries, uni_to_multivar_binary_index,
+        MultilinearAsUnivariate,
+    },
 };
 const K: u32 = 17;
 const N_CURRENCIES: usize = 2;
 const N_USERS: usize = 1 << 16;
-
-pub fn run_plonkish_backend<F, Pb, T, C>(
-    num_vars: usize,
-    circuit_fn: impl Fn(usize) -> (PlonkishCircuitInfo<F>, C),
-) where
-    F: PrimeField + Hash + Serialize + DeserializeOwned,
-    Pb: PlonkishBackend<F>,
-    T: TranscriptRead<<Pb::Pcs as PolynomialCommitmentScheme<F>>::CommitmentChunk, F>
-        + TranscriptWrite<<Pb::Pcs as PolynomialCommitmentScheme<F>>::CommitmentChunk, F>
-        + InMemoryTranscript<Param = ()>,
-    C: PlonkishCircuit<F>,
-{
-    let (circuit_info, circuit) = circuit_fn(num_vars);
-    let instances = circuit.instances();
-
-    let param = Pb::setup(&circuit_info, seeded_std_rng()).unwrap();
-
-    let (pp, vp) = Pb::preprocess(&param, &circuit_info).unwrap();
-
-    let proof = {
-        let mut transcript = T::new(());
-        Pb::prove(&pp, &circuit, &mut transcript, seeded_std_rng()).unwrap();
-        transcript.into_proof()
-    };
-
-    let result = {
-        let mut transcript = T::from_proof((), proof.as_slice());
-        Pb::verify(&vp, instances, &mut transcript, seeded_std_rng())
-    };
-    assert_eq!(result, Ok(()));
-
-    let wrong_instances = instances[0]
-        .iter()
-        .map(|instance| *instance + F::ONE)
-        .collect::<Vec<_>>();
-    let wrong_result = {
-        let mut transcript = T::from_proof((), proof.as_slice());
-        Pb::verify(
-            &vp,
-            &vec![wrong_instances],
-            &mut transcript,
-            seeded_std_rng(),
-        )
-    };
-    assert_eq!(
-        wrong_result,
-        Err(InvalidSumcheck(
-            "Consistency failure at round 1".to_string()
-        ))
-    );
-}
 
 pub fn seeded_std_rng() -> impl RngCore + CryptoRng {
     StdRng::seed_from_u64(OsRng.next_u64())
@@ -81,20 +35,210 @@ pub fn seeded_std_rng() -> impl RngCore + CryptoRng {
 
 #[test]
 fn test_summa_hyperplonk() {
-    type Pb = HyperPlonk<MultilinearKzg<Bn256>>;
+    type ProvingBackend = HyperPlonk<MultilinearKzg<Bn256>>;
     let entries = generate_dummy_entries::<N_USERS, N_CURRENCIES>().unwrap();
     let circuit = SummaHyperplonk::<N_USERS, N_CURRENCIES>::init(entries.to_vec());
     let num_vars = K;
-    run_plonkish_backend::<Fp, Pb, Keccak256Transcript<_>, _>(
-        num_vars.try_into().unwrap(),
-        |num_vars| {
-            let circuit = Halo2Circuit::<Fp, SummaHyperplonk<N_USERS, N_CURRENCIES>>::new::<Pb>(
-                num_vars,
-                circuit.clone(),
-            );
-            (circuit.circuit_info().unwrap(), circuit)
-        },
+
+    let circuit_fn = |num_vars| {
+        let circuit = Halo2Circuit::<Fp, SummaHyperplonk<N_USERS, N_CURRENCIES>>::new::<
+            ProvingBackend,
+        >(num_vars, circuit.clone());
+        (circuit.circuit_info().unwrap(), circuit)
+    };
+
+    let (circuit_info, circuit) = circuit_fn(num_vars as usize);
+    let instances = circuit.instances();
+
+    let param = ProvingBackend::setup(&circuit_info, seeded_std_rng()).unwrap();
+
+    let (prover_parameters, verifier_parameters) =
+        ProvingBackend::preprocess(&param, &circuit_info).unwrap();
+
+    let (witness_polys, proof_transcript) = {
+        let mut proof_transcript = Keccak256Transcript::new(());
+
+        let witness_polys = ProvingBackend::prove(
+            &prover_parameters,
+            &circuit,
+            &mut proof_transcript,
+            seeded_std_rng(),
+        )
+        .unwrap();
+        (witness_polys, proof_transcript)
+    };
+
+    let num_points = N_CURRENCIES + 1;
+
+    let proof = proof_transcript.into_proof();
+
+    let mut transcript;
+    let result: Result<(), plonkish_backend::Error> = {
+        transcript = Keccak256Transcript::from_proof((), proof.as_slice());
+        ProvingBackend::verify(
+            &verifier_parameters,
+            instances,
+            &mut transcript,
+            seeded_std_rng(),
+        )
+    };
+    assert_eq!(result, Ok(()));
+
+    let invalid_grand_total_instances = instances[0]
+        .iter()
+        .enumerate()
+        .map(|(i, element)| {
+            if i == 0 {
+                *element
+            } else {
+                Fp::random(seeded_std_rng())
+            }
+        })
+        .collect::<Vec<_>>();
+
+    let invalid_result = {
+        let mut transcript = Keccak256Transcript::from_proof((), proof.as_slice());
+        ProvingBackend::verify(
+            &verifier_parameters,
+            &[invalid_grand_total_instances],
+            &mut transcript,
+            seeded_std_rng(),
+        )
+    };
+    assert_eq!(
+        invalid_result,
+        Err(InvalidSumcheck(
+            "Consistency failure at round 1".to_string()
+        ))
     );
+
+    let invalid_range_check_instances = instances[0]
+        .iter()
+        .enumerate()
+        .map(|(i, element)| {
+            if i == 0 {
+                Fp::random(seeded_std_rng())
+            } else {
+                *element
+            }
+        })
+        .collect::<Vec<_>>();
+
+    let invalid_result = {
+        let mut transcript = Keccak256Transcript::from_proof((), proof.as_slice());
+        ProvingBackend::verify(
+            &verifier_parameters,
+            &[invalid_range_check_instances],
+            &mut transcript,
+            seeded_std_rng(),
+        )
+    };
+    assert_eq!(
+        invalid_result,
+        Err(InvalidSumcheck(
+            "Consistency failure at round 1".to_string()
+        ))
+    );
+
+    //Create an evaluation challenge at a random "user index"
+    let fraction: f64 = rand::thread_rng().gen();
+    let random_user_index = (fraction * (entries.len() as f64)) as usize;
+
+    assert_eq!(
+        fp_to_big_uint(&witness_polys[0].evaluate_as_univariate(&random_user_index)),
+        *entries[random_user_index].username_as_big_uint()
+    );
+    assert_eq!(
+        fp_to_big_uint(&witness_polys[1].evaluate_as_univariate(&random_user_index)),
+        entries[random_user_index].balances()[0]
+    );
+
+    // Convert challenge into a multivariate form
+    let multivariate_challenge: Vec<Fp> =
+        uni_to_multivar_binary_index(&random_user_index, num_vars as usize);
+
+    let mut kzg_transcript = Keccak256Transcript::new(());
+
+    let mut transcript = Keccak256Transcript::from_proof((), proof.as_slice());
+
+    let user_entry_commitments = MultilinearKzg::<Bn256>::read_commitments(
+        &verifier_parameters.pcs,
+        num_points,
+        &mut transcript,
+    )
+    .unwrap();
+    let user_entry_polynomials = witness_polys.iter().take(num_points).collect::<Vec<_>>();
+
+    //Store the user index multi-variable in the transcript for the verifier
+    for binary_var in multivariate_challenge.iter() {
+        kzg_transcript.write_field_element(binary_var).unwrap();
+    }
+
+    let evals = user_entry_polynomials
+        .iter()
+        .enumerate()
+        .map(|(poly_idx, poly)| {
+            Evaluation::new(poly_idx, 0, poly.evaluate(&multivariate_challenge))
+        })
+        .collect_vec();
+
+    MultilinearKzg::<Bn256>::batch_open(
+        &prover_parameters.pcs,
+        user_entry_polynomials,
+        &user_entry_commitments,
+        &[multivariate_challenge],
+        &evals,
+        &mut kzg_transcript,
+    )
+    .unwrap();
+
+    let kzg_proof = kzg_transcript.into_proof();
+
+    // Verifier side
+    let mut kzg_transcript = Keccak256Transcript::from_proof((), kzg_proof.as_slice());
+
+    // The verifier knows the ZK-SNARK proof, can extract the polynomial commitments
+    let mut transcript = Keccak256Transcript::from_proof((), proof.as_slice());
+    let user_entry_commitments = MultilinearKzg::<Bn256>::read_commitments(
+        &verifier_parameters.pcs,
+        num_points,
+        &mut transcript,
+    )
+    .unwrap();
+
+    //The verifier doesn't know the mapping of their "user index" to the multi-variable index, reads it from the transcript
+    let mut multivariate_challenge: Vec<Fp> = Vec::new();
+    for _ in 0..num_vars {
+        multivariate_challenge.push(kzg_transcript.read_field_element().unwrap());
+    }
+
+    //The user knows their evaluation at the challenge point
+    let evals: Vec<Evaluation<Fp>> = (0..N_CURRENCIES + 1)
+        .map(|i| {
+            if i == 0 {
+                Evaluation::new(
+                    i,
+                    0,
+                    big_uint_to_fp::<Fp>(entries[random_user_index].username_as_big_uint()),
+                )
+            } else {
+                Evaluation::new(
+                    i,
+                    0,
+                    big_uint_to_fp::<Fp>(&entries[random_user_index].balances()[i - 1]),
+                )
+            }
+        })
+        .collect();
+
+    MultilinearKzg::<Bn256>::batch_verify(
+        &verifier_parameters.pcs,
+        &user_entry_commitments,
+        &[multivariate_challenge],
+        &evals,
+        &mut kzg_transcript,
+    )
+    .unwrap();
 }
 
 #[cfg(feature = "dev-graph")]
